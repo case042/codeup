dfc18cdcd5b14ad77f8ba6396c66930873d66835
case042 <smileboy0318@naver.com>
2022-02-05 13:46:15 +0900

    zzzz

---
+++ 6070.py
@@ -0,0 +1,10 @@
+a = int(input())
+
+if a//3 ==1:
+    print('spring')
+elif a==6 or a==7 or a==8:
+    print('summer')
+elif a == 9 or a==10 or a==11:
+    print('fall')
+else:
+    print('winter')
